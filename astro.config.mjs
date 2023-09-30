cd0f5ef76a0bcf36084f88ba071f0ebebac4c942
Elvann <elvann.abendroth@gmail.com>
2023-09-30 13:28:28 +0300

    🧪site astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://oodri-dev.vercel.app/",
+  site: "https://oodri-dev-git-url-search-params-elvannabendroth.vercel.app/",
   integrations: [tailwind(), mdx(), sitemap(), react()],
   //output & adapter make site SSR and allows searchParams
 
