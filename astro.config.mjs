5372cdf04d3b52be62bd61b156d5d16959c9cd04
Elvann <elvann.abendroth@gmail.com>
2024-05-28 23:57:13 +0300

    ⚙️redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
     "/portfolio": "/projects",
     "/portfolio/[slug]": "/projects/[slug]",
     "/resume/hg":
-      "https://www.dropbox.com/scl/fi/yh92xauqwtlcgnrrv49sv/Audrey-Downey-Resume-2024-May-26.pdf?rlkey=edotjjxtz78ydbamve1i82d2m&dl=0",
+      "https://www.dropbox.com/scl/fi/k6kos15w5phxd2l9qjas6/Audrey-Downey-Resume-2024-May-28-HG.pdf?rlkey=ueweesplzconbutb4nyfouewz&dl=0",
     "/pdf/trustme":
       "https://www.dropbox.com/scl/fi/z8wscrl90s15g6bief0b6/Trust-Me-I-m-an-Engineer-Branding.pdf?rlkey=7o6fbaq9zt81wkobwxb4tcryo&dl=0",
     "/resume/andc":
